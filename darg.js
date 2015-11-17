db44df774763a6233b12cd227b8f566533f3b64c
happy365lyzh <503887183@qq.com>
2015-11-17 21:25:42 +0800

    chage1 darg.html.js

--- darg.js
+++ darg.js
@@ -1,3 +1,9 @@
-function darg(){
-	
-}
+window.onload = function(){
+	var oDivs = document.getElementsByTagName("div");
+	oDivs[0].style.width = '50px';
+	oDivs[0].style.height = '50px';
+	oDivs[0].style.background = 'red';
+	oDivs[0].onclick = function(){
+		alert('dddd');
+	};
+};
